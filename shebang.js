05b9abdbe47388626564c8be256755eae598ea2b
Star Brilliant <m13253@hotmail.com>
2012-10-07 00:25:57 +0800

    Add shebang.js

---
+++ shebang.js
@@ -0,0 +1,11 @@
+
+function getshebang() {
+    var she = location.hash;
+    if(!she)
+        return undefined;
+    var idx = she.indexOf('!');
+    if(idx==-1)
+        return undefined;
+    return idx.substr(idx+1);
+}
+
