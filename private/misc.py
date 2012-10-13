11fa043afa7c442b4818e131759fa10ded97787a
Star Brilliant <m13253@hotmail.com>
2012-10-13 17:44:03 +0800

    Moved to private

--- private/misc.py
+++ private/misc.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import os
 import sys
+import config
 
 def include(ifilename, ostream=sys.stdout):
     with open(ifilename, 'rb') as istream:
@@ -16,4 +18,10 @@ def writebin(istr, ostream=sys.stdout):
         return ostream.buffer.write(istr)
     else:
         return ostream.buffer.write(istr.encode('utf-8', 'replace'))
+
+def privfile(filename=None):
+    if filename==None:
+        return config.private_dir
+    else:
+        return os.path.join(config.private_dir, filename);
 
--- public/play.py
+++ public/play.py
@@ -2,6 +2,10 @@
 
 import config
 import misc
+import os
 
 misc.writebin("Status: 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n");
-misc.include("play.html")
+misc.include(misc.privfile("htmlhead.html"))
+misc.include(misc.privfile("banner.html"))
+misc.include(misc.privfile("play.html"))
+misc.include(misc.privfile("htmlfoot.html"))
