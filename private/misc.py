4ca36e9c7b9d251f816a8e918946ec9215a62b0c
Star Brilliant <m13253@hotmail.com>
2012-10-13 13:39:10 +0800

    Update misc.py

--- private/misc.py
+++ private/misc.py
@@ -3,11 +3,17 @@
 import sys
 
 def include(ifilename, ostream=sys.stdout):
-    with open(ifilename, "rb") as istream:
+    with open(ifilename, 'rb') as istream:
         while True:
             buf=istream.read(4096)
             if buf:
-                ostream.buffer.write(buf)
+                writebin(buf, ostream)
             else:
                 break
+
+def writebin(istr, ostream=sys.stdout):
+    if isinstance(istr, bytes):
+        return ostream.buffer.write(istr)
+    else:
+        return ostream.buffer.write(istr.encode('utf-8', 'replace'))
 
