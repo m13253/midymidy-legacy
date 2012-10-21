b3d256e82e0ac21fb33be1d544b7cd2fbf2271b8
Star Brilliant <m13253@hotmail.com>
2012-10-21 16:23:54 +0800

    Fix mistype

--- public/play.py
+++ public/play.py
@@ -36,7 +36,7 @@ def main():
     '''<section itemscope itemtype="http://schema.org/MusicRecording">
 <div style="width: 100%; font-size: 32pt; font-weight: bold"><span itemprop="name">'''+escape_for_html(musicdata['title'])+'''</span></div>
 <div style="width: 100%; text-align: right">发布时间：<span><meta itemprop="datePublished" content="'''+
-    escape_for_html(time.strftime("Y-%m-%dT%H:%M:%S%z", time.gmtime(musicdata['time'])))+
+    escape_for_html(time.strftime("%Y-%m-%dT%H:%M:%S%z", time.gmtime(musicdata['time'])))+
     '''" />'''+
     escape_for_html(time.strftime("%c", time.localtime(musicdata['time'])))+
     '''</span>，发布者&nbsp;ID：<span itemprop="author">'''+escape_for_html(musicdata['uploader'])
