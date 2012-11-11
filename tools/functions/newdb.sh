380ffb9431f892d7bc775e53c40150436aaeb71b
Star Brilliant <m13253@hotmail.com>
2012-11-12 00:43:17 +0800

    Fix 'echo -e \t' with 'echo <Tab>' for dash

--- tools/functions/newdb.sh
+++ tools/functions/newdb.sh
@@ -8,7 +8,7 @@ then
     cat | sqlite3 "$1.new"
     chmod 666 "$1.new"
     mv "$1.new" "$1"
-    echo -e '\tOK.'
+    echo '	OK.'
 else
     echo 'This is a databace creation tool, it is called by createdb.sh.'
     echo 'Do not execute it manually.'
